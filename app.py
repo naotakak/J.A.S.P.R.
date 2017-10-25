efdf66e00719591d063953a56fbe98cfe3c8ce58
Anish2454 <shenoy.anish.227@gmail.com>
2017-10-25 13:26:10 -0400

    Added stufff

--- app.py
+++ app.py
@@ -1,15 +1,37 @@
 from flask import Flask, render_template, redirect, url_for, request, session
 import os
-import util.story_db
 
 app = Flask(__name__)
 
 #genrerates random key
 app.secret_key = os.urandom(32)
 
+'''
+if true, can display the entire story
+if false, can only see the latest commit
+def can_view(user_id, story_id):
+	stories = db.get_list(user_id)
+	return story_id in stories 
+'''
+
+'''
+#Accounts database Functions
+def check_account(username):
+	command = "SELECT username FROM accounts"
+	usernames = c.execute(command)
+	for list_username in usernames:
+		if list_username == username:
+			return False
+	return True
+	
+def create_account(username, password):
+	command = "INSERT INTO accounts VALUES (" + username + ", " + password + ")" 
+'''
+
+
 @app.route("/")
 def root():
-	return "hi"
+	return "hi" 
 
 @app.route("/home")
 def home():
@@ -18,11 +40,11 @@ def home():
 @app.route("/login")
 def login():
 	return
-
+	
 @app.route("/create_account")
 def create_account():
 	return
-
+	
 @app.route("/view_contr_stories")
 def view_contr_stories():
 	return
@@ -31,19 +53,20 @@ def view_contr_stories():
 @app.route("/create_story")
 def create_story():
 	return
-
+	
 @app.route("/index_stories")
 def index_stories():
 	return
-
+	
 @app.route("/edit")
 def edit_story():
 	return
 
+	
 if __name__ == '__main__':
     app.debug = True
     app.run()
+	
 
-
-
+	
 
