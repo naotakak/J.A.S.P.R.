1fb4d73d3e414e3ef533799aee5cfa3158b073dc
Naotaka Kinoshita <naotakakinoshita@gmail.com>
2017-10-25 12:54:44 -0400

    update devlog and story_db.py

--- util/story_db.py
+++ util/story_db.py
@@ -3,7 +3,7 @@
 import sqlite3
 from os.path import isfile
 
-path = "../data/stories.db"
+path = "data/stories.db"
 
 def get_db():
 	return sqlite3.connect(path)
@@ -16,7 +16,7 @@ def get_cursor(db):
 if isfile(path):
 	pass
 else:
-	# db = sqlite3.connect(path)
-	# cmd = "CREATE TABLE stories(id INT PRIMARY KEY, title TEXT, rest_of_text TEXT, last_text TEXT);"
-	# db.execute(cmd)
+	db = sqlite3.connect(path)
+	cmd = "CREATE TABLE stories(id INT PRIMARY KEY, title TEXT, rest_of_text TEXT, last_text TEXT);"
+	db.execute(cmd)
 	print "makes database here"
