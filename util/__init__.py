e39a12e8b2cc7222ea74ec57a417464cdfb1c2dc
ykadir42 <ykadir42@users.noreply.github.com>
2017-10-26 12:46:01 -0400

    Conditional initialization

--- util/__init__.py
+++ util/__init__.py
@@ -1,17 +1,19 @@
-
 import sqlite3
 from os.path import isfile
-path = "../data/data.db"
+path = "data/data.db"
 def creato():
-    db = sqlite3.connect(path)
-    cmd_story = "CREATE TABLE stories(id INT PRIMARY KEY, title TEXT,\
-    rest_of_text TEXT, last_text TEXT);"
-    cmd_account = "CREATE TABLE account(Username TEXT,Password TEXT,\
-    STORIES TEXT, id INT PRIMARY KEY);"
-    
-    db.execute(cmd_story)
-    db.execute(cmd_account)
-    print "makes database here"
+	if isfile(path):
+		pass
+	else:
+		db = sqlite3.connect(path)
+		cmd_story = "CREATE TABLE stories(id INT PRIMARY KEY, title TEXT,\
+		rest_of_text TEXT, last_text TEXT);"
+		cmd_account = "CREATE TABLE account(Username TEXT,Password TEXT,\
+		STORIES TEXT, id INT PRIMARY KEY);"
+
+		db.execute(cmd_story)
+		db.execute(cmd_account)
+		print "makes database here"
 
 
 creato()
