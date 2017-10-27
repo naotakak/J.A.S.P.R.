7b31589b2c0894d80319df34568bbf6ff3a7b063
ykadir42 <ykadir42@gmail.com>
2017-10-27 00:32:55 -0400

    Added test cases for story db functions

--- db_test.py
+++ db_test.py
@@ -1,2 +1,17 @@
-import util
+from util import db
 
+db.create_story(0, "The Angry Hare", "Once there was a meme that lived.")
+db.create_story(1, "Sasketchuan", "Bigfoot lived in Canadia.")
+print "============get_ids============"
+print db.get_ids()
+"============get_story============"
+print db.get_story(0)
+"============get_last============"
+print db.get_last(0)
+db.update_story(0, "Then it died.")
+"============get_ids============"
+print db.get_ids()
+"============get_story============"
+print db.get_story(0)
+"============get_last============"
+print db.get_last(0)
